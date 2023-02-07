f099acef6bde2c9ea23ef24600a604955505bd60
Nirmal <nirmalraj1025@gmail.com>
2023-02-07 13:10:41 +0530

    profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Home from "./Pages/Home/Home";
-// import Profile from "./Pages/Profile/Profile";
+import Profile from "./Pages/Profile/Profile";
 
 function App() {
   return (
     <div>
       <Home />
-      {/* <Profile /> */}
+      <Profile />
     </div>
   );
 }
